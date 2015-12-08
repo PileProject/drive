1e29e9123453c83df89b1d9ef58b65e538b69608
tiwanari <tatsuyaw0c@gmail.com>
2015-12-08 15:45:58 +0900

    refactor NxtPortConnectionFragment

--- app/src/nxt/java/com/pileproject/drive/setting/machine/NxtPortConnectionFragment.java
+++ app/src/nxt/java/com/pileproject/drive/setting/machine/NxtPortConnectionFragment.java
@@ -14,7 +14,6 @@ import com.pileproject.drive.util.SharedPreferencesWrapper;
 import com.pileproject.drive.view.PortTextView;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -22,50 +21,45 @@ import java.util.Map.Entry;
 
 
 public class NxtPortConnectionFragment extends Fragment {
-
 	private Activity mParentActivity;
 	private View mRootView;
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		super.onCreateView(inflater, container, savedInstanceState);
 
 		mRootView = inflater.inflate(R.layout.fragment_nxtportconfig, container, false);
-		
 		return mRootView;
 	}
-	
+
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		
+
 		mParentActivity = getActivity();
 		Context context = mParentActivity.getApplicationContext();
-
 
 		setDefaultValueOnPreferences(context);
 		initializeSensorPorts(context, mRootView);
 		initializeMotorPorts(context, mRootView);
 	}
-	
+
 	public static void setDefaultValueOnPreferences(Context context) {
 		Map<String, Integer> preferences = new HashMap<String, Integer>();
-		
+
 		preferences.put("sensorPort1", NxtController.SensorProperty.SENSOR_TOUCH);
 		preferences.put("sensorPort2", NxtController.SensorProperty.SENSOR_SOUND);
 		preferences.put("sensorPort3", NxtController.SensorProperty.SENSOR_LINE);
 		preferences.put("motorPortB", NxtController.MotorProperty.MOTOR_LEFT);
 		preferences.put("motorPortC", NxtController.MotorProperty.MOTOR_RIGHT);
-		
-		for (Iterator<Entry<String, Integer>> itr = preferences.entrySet().iterator(); itr.hasNext(); ) {
-			Map.Entry<String, Integer> entry = itr.next();
-			
+
+		for (Entry<String, Integer> entry : preferences.entrySet()) {
 			if (SharedPreferencesWrapper.loadIntPreference(context, entry.getKey(), -1) == -1) {
 				SharedPreferencesWrapper.saveIntPreference(context, entry.getKey(), entry.getValue());
 			}
 		}
 	}
-	
+
 	private void initializeSensorPorts(Context context, View root) {
 		final int[] sensorPortIds = {
 				R.id.setting_portconfig_sensorPort1,
@@ -73,7 +67,7 @@ public class NxtPortConnectionFragment extends Fragment {
 				R.id.setting_portconfig_sensorPort3,
 				R.id.setting_portconfig_sensorPort4,
 		};
-		
+
 		final int[] sensorPlaceIds = {
 				R.id.setting_portconfig_sensor1,
 				R.id.setting_portconfig_sensor2,
@@ -81,23 +75,27 @@ public class NxtPortConnectionFragment extends Fragment {
 		};
 
 		LinkedList<Integer> sensorsInUsed = new LinkedList<Integer>();
-		List<Integer> allSensors = NxtController.SensorProperty.getAllSensors();
 		for (int i = 0; i < NxtController.SensorProperty.NUMBER_OF_SENSOR_PORTS; ++ i) {
 			PortTextView portTextView = (PortTextView) root.findViewById(sensorPortIds[i]);
-			int sensorType = SharedPreferencesWrapper.loadIntPreference(context, portTextView.getPortName(), NxtController.SensorProperty.SENSOR_UNUSED);
-			
+			int sensorType =
+					SharedPreferencesWrapper
+							.loadIntPreference(context,
+									portTextView.getPortName(),
+									NxtController.SensorProperty.SENSOR_UNUSED);
+
 			if (sensorType != NxtController.SensorProperty.SENSOR_UNUSED) {
 				portTextView.setAttachmentType(sensorType);
 				sensorsInUsed.add(sensorType);
 			}
 		}
-		
+
+		List<Integer> allSensors = NxtController.SensorProperty.getAllSensors();
 		int index = 0;
-		for (Iterator<Integer> itr = allSensors.iterator(); itr.hasNext(); ) {
-			Integer sensorType = itr.next();
-			
+		for (Integer sensorType : allSensors) {
 			if (sensorsInUsed.indexOf(sensorType) == -1) {
-				((PortTextView) root.findViewById(sensorPlaceIds[index++])).setAttachmentType(sensorType);
+				((PortTextView) root
+						.findViewById(sensorPlaceIds[index++]))
+						.setAttachmentType(sensorType);
 			}
 		}
 	}
@@ -108,30 +106,34 @@ public class NxtPortConnectionFragment extends Fragment {
 				R.id.setting_portconfig_motorPortB,
 				R.id.setting_portconfig_motorPortC,
 		};
-		
+
 		final int[] motorPlaceIds = {
 				R.id.setting_portconfig_motor1,
 				R.id.setting_portconfig_motor2,
 		};
 
 		LinkedList<Integer> motorsInUsed = new LinkedList<Integer>();
-		List<Integer> allMotors = NxtController.MotorProperty.getAllMotors();
 		for (int i = 0; i < NxtController.MotorProperty.NUMBER_OF_MOTOR_PORTS; ++ i) {
 			PortTextView portTextView = (PortTextView) root.findViewById(motorPortIds[i]);
-			int motorType = SharedPreferencesWrapper.loadIntPreference(context, portTextView.getPortName(), NxtController.MotorProperty.MOTOR_UNUSED);
-			
+			int motorType =
+					SharedPreferencesWrapper
+							.loadIntPreference(context,
+									portTextView.getPortName(),
+									NxtController.MotorProperty.MOTOR_UNUSED);
+
 			if (motorType != NxtController.MotorProperty.MOTOR_UNUSED) {
 				portTextView.setAttachmentType(motorType);
 				motorsInUsed.add(motorType);
 			}
 		}
 
+		List<Integer> allMotors = NxtController.MotorProperty.getAllMotors();
 		int index = 0;
-		for (Iterator<Integer> itr = allMotors.iterator(); itr.hasNext(); ) {
-			Integer motorType = itr.next();
-			
+		for (Integer motorType : allMotors) {
 			if (motorsInUsed.indexOf(motorType) == -1) {
-				((PortTextView) root.findViewById(motorPlaceIds[index++])).setAttachmentType(motorType);
+				((PortTextView) root
+						.findViewById(motorPlaceIds[index++]))
+						.setAttachmentType(motorType);
 			}
 		}
 	}
