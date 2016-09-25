d480c082777b37b43ead4a1db9773761ec8c801c
tiwanari <tatsuyaw0c@gmail.com>
2016-09-25 22:07:14 +0900

    Allow to choose the firmware of nxt

--- app/src/nxt/java/com/pileproject/drive/programming/visual/block/selection/IfNxtWasTouchedBlock.java
+++ app/src/nxt/java/com/pileproject/drive/programming/visual/block/selection/IfNxtWasTouchedBlock.java
@@ -23,6 +23,8 @@ import com.pileproject.drive.R;
 import com.pileproject.drive.execution.ExecutionCondition;
 import com.pileproject.drive.execution.MachineController;
 import com.pileproject.drive.execution.NxtController;
+import com.pileproject.drive.preferences.MachinePreferences;
+import com.pileproject.drive.preferences.MachinePreferencesSchema;
 
 /**
  * This block check the touch sensor's value and check it was touched or not
@@ -31,15 +33,22 @@ import com.pileproject.drive.execution.NxtController;
  * @version 1.0 7-July-2013
  */
 public class IfNxtWasTouchedBlock extends SelectionBlock {
-
+    private String mFirmware;
     public IfNxtWasTouchedBlock(Context context) {
         super(context);
         LayoutInflater.from(context).inflate(R.layout.block_if_nxt_was_touched, this);
+
+        mFirmware = MachinePreferences.get(getContext()).getFirmware();
     }
 
     @Override
     public int action(MachineController controller, ExecutionCondition condition) {
-        condition.pushSelectionResult(((NxtController) controller).getTouchSensorValue());
+        boolean wasTouched = ((NxtController) controller).getTouchSensorValue();
+
+        // leJOS returns the opposite value
+        if (mFirmware.equals(MachinePreferencesSchema.FIRMWARE.LEJOS)) wasTouched = !wasTouched;
+
+        condition.pushSelectionResult(wasTouched);
         return 0;
     }
 }
---
+++ app/src/nxt/java/com/pileproject/drive/setting/machine/NxtFirmwareFragment.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2011-2015 PILE Project, Inc. <dev@pileproject.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pileproject.drive.setting.machine;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+
+import com.pileproject.drive.R;
+import com.pileproject.drive.preferences.MachinePreferences;
+
+import static com.pileproject.drive.preferences.MachinePreferencesSchema.FIRMWARE.LEJOS;
+import static com.pileproject.drive.preferences.MachinePreferencesSchema.FIRMWARE.STANDARD;
+
+public class NxtFirmwareFragment extends DialogFragment {
+    private RadioGroup mFirmwareRadioGroup = null;
+
+    @Override
+    @NonNull
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = new Dialog(getActivity());
+        dialog.setTitle(R.string.setting_firmware);
+        return dialog;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View v = inflater.inflate(R.layout.fragment_nxt_firmware, container, false);
+        mFirmwareRadioGroup = (RadioGroup) v.findViewById(R.id.firmware_firmwares);
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        mFirmwareRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.firmware_standard:
+                        MachinePreferences.get(getActivity()).setFirmware(STANDARD);
+                        break;
+                    case R.id.firmware_lejos:
+                        MachinePreferences.get(getActivity()).setFirmware(LEJOS);
+                        break;
+                }
+            }
+        });
+
+        String firm = MachinePreferences.get(getActivity()).getFirmware();
+        if (firm.equals(STANDARD))
+            mFirmwareRadioGroup.check(R.id.firmware_standard);
+        else
+            mFirmwareRadioGroup.check(R.id.firmware_lejos);
+
+    }
+}
