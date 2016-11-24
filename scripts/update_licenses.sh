c0a81fd6adee9572260cded8fb1e43b3f8d861a2
tiwanari <tatsuyaw0c@gmail.com>
2016-11-24 14:57:18 +0900

    remove scripts folder

--- scripts/update_licenses.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-license_output=./app/build/reports/license/dependency-license.html
-assets_folder=./app/src/main/assets/
-
-cd ..
-./gradlew downloadLicenses
-# cat ./app/build/reports/license/license-dependency.xml
-
-cp $license_output $assets_folder/license.html
