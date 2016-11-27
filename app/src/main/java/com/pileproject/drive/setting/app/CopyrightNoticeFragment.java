e1907df573a31539ab1755912d33d02db29e4d14
Tatsuya Iwanari <tiwanari@users.noreply.github.com>
2016-11-28 08:47:47 +0900

    Add Copyright Notice (#57)
    * add icense-gradle-plugin
    
    * add CopyrightNoticeFragment and a simple script (WIP)
    
    * update LICENSE and refer it from gradle
    
    * update the headers of Java files by using licenseFormat command
    
    * refactor .travis.yml and change the android target version
    
    * add tasks to clean up licenses
    
    * rename license files
    
    * replace the plugin to make licenses.html with cookpad's one
    
    * remove scripts folder
    
    * readd license-gradle-plugin
    
    * update xml's licenses
    
    * ignore *Test.java for license checking
    
    * fix an error on app/src/main/res/layout/activity_setting.xml
    
    * fix an error on app/src/main/res/layout/content_execution.xml

---
+++ app/src/main/java/com/pileproject/drive/setting/app/CopyrightNoticeFragment.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (C) 2011-2016 PILE Project, Inc. <dev@pileproject.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.pileproject.drive.setting.app;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.webkit.WebView;
+
+import com.pileproject.drive.R;
+
+public class CopyrightNoticeFragment extends DialogFragment {
+    WebView mLicensesPage;
+
+    @Override
+    @NonNull
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = new Dialog(getActivity());
+        dialog.setTitle(R.string.setting_copyrightNotice);
+        return dialog;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        View v = inflater.inflate(R.layout.fragment_copyright_notice, container, false);
+        mLicensesPage = (WebView) v.findViewById(R.id.licenses_page);
+
+        // load a license file located in assets
+        String htmlPath = "file:///android_asset/licenses.html";
+        mLicensesPage.loadUrl(htmlPath);
+
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        resizeDialog();
+    }
+
+    /**
+     * This function should be called in {@link DialogFragment#onActivityCreated(Bundle)}.
+     * Otherwise, the dialog size will never be changed
+     */
+    private void resizeDialog() {
+        Dialog dialog = getDialog();
+
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+
+        // resize window large enough to display list views
+        int dialogWidth = (int) (metrics.widthPixels * 0.9);
+        int dialogHeight = (int) (metrics.heightPixels * 0.9);
+
+        WindowManager.LayoutParams lp = dialog.getWindow().getAttributes();
+        lp.width = dialogWidth;
+        lp.height = dialogHeight;
+        dialog.getWindow().setAttributes(lp);
+    }
+}
