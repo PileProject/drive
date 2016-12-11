9b9dcdcd821b3c956581e4723710901017b85ebb
Tatsuya Iwanari <tiwanari@users.noreply.github.com>
2016-12-11 14:44:40 +0900

    Apply the license format to tests (#58)

--- app/src/androidTest/java/com/pileproject/drive/programming/visual/block/NumberTextViewDelegateTest.java
+++ app/src/androidTest/java/com/pileproject/drive/programming/visual/block/NumberTextViewDelegateTest.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (C) 2011-2015 PILE Project, Inc. <dev@pileproject.com>
+/**
+ * Copyright (C) 2011-2016 PILE Project, Inc. <dev@pileproject.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.pileproject.drive.programming.visual.block;
 
 import android.support.test.runner.AndroidJUnit4;
